736c6ae361e9ca6b70c0fcb5f7668a6047c8f2e2
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-03-12 20:40:42 -0400

    commit tree 23c3748a42fa81c18f6dcdb09824b94f0cfd76fa

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 1, "M", "Brown", "Sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Black", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Honey", "Sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Brown", "Mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "F", "Black", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "M", "White", "Mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "White", "Sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", "Black", "Sleepy", 1);
